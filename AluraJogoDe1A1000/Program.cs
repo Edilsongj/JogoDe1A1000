56cbfd377f802ad2aef61651ede90dc200c917b1
Arnaux <arnauxx@hotmail.com>
2025-07-13 13:18:24 -0300

    Remoção de comentários

--- AluraJogoDe1A1000/Program.cs
+++ AluraJogoDe1A1000/Program.cs
@@ -2,11 +2,8 @@
 
 class Program {
     static void Main() {
-        // Máquina” de sorteio com um número aleatório.
         Random Sorteador = new Random();
-        //variável pra guardar o número sorteado
         int NumeroSecreto = Sorteador.Next(1, 1001);
-        //variável para guardar o chute do usuário
         int chutes = 0;
 
         do
@@ -16,7 +13,6 @@ class Program {
             string entrada = (Console.ReadLine()!);
             chutes = int.Parse(entrada);
 
-            // Verifica se o chute é válido
             if (chutes > NumeroSecreto)
             {
                 Console.WriteLine("É menor");
