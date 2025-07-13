c373b586ac6af9e61157398824c9da98f206b94e
Edilson Guimarães <eddy.g.jesus@gmail.com>
2025-07-13 13:40:54 -0300

    remoção de classes desnecessarias pt2.

--- AluraJogoDe1A1000/Program.cs
+++ AluraJogoDe1A1000/Program.cs
@@ -1,30 +1,29 @@
-﻿class Program {
-    static void Main() {
-        Random Sorteador = new Random();
-        int NumeroSecreto = Sorteador.Next(1, 1001);
-        int chutes = 0;
+﻿
+Random Sorteador = new Random();
+int NumeroSecreto = Sorteador.Next(1, 1001);
+int chutes = 0;
 
-        do
-        {
+do
+{
 
-            Console.WriteLine("Digite seu palpite" );
-            string entrada = (Console.ReadLine()!);
-            chutes = int.Parse(entrada);
+    Console.WriteLine("Digite seu palpite");
+    string entrada = (Console.ReadLine()!);
+    chutes = int.Parse(entrada);
 
-            if (chutes > NumeroSecreto)
-            {
-                Console.WriteLine("É menor");
-            }
-            else if (chutes < NumeroSecreto)
-            {
-                Console.WriteLine("É maior");
-
-            }
-            else {
-                Console.WriteLine("Parabéns! Você acertou.");
-            }
-
-        }while (chutes != NumeroSecreto);
+    if (chutes > NumeroSecreto)
+    {
+        Console.WriteLine("É menor");
+    }
+    else if (chutes < NumeroSecreto)
+    {
+        Console.WriteLine("É maior");
 
     }
-}
+    else
+    {
+        Console.WriteLine("Parabéns! Você acertou.");
+    }
+
+} while (chutes != NumeroSecreto);
+
+
